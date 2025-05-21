a53af224e80a55b9a5780ce0c4cd5c0c44edca24
Simionoff Carmina <cb.simionoff@gmail.com>
2025-05-21 17:12:22 +0300

    Added Latest Products area on Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LatestProducts from '../components/LatestProducts';
 
 function HomePage() {
   return (
     <div>
       <h1>Welcome to the Online Marketplace</h1>
       <p>Browse products, categories, and more!</p>
+      <LatestProducts />
     </div>
   );
 }
