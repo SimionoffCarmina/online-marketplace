509dd3204ef0739a63c59c23b38dfec935036633
Simionoff Carmina <cb.simionoff@gmail.com>
2025-05-22 17:35:57 +0300

    Created create account page with validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import AccountPage from './pages/AccountPage';
+import CreateAccountPage from './pages/CreateAccountPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/signup" element={<CreateAccountPage />} />
       </Routes>
     </Router>
   );
